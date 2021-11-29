1653ce5f14fefaf0c34736f4212258ac2450915f
CatherineLouise18 <35455850+CatherineLouise18@users.noreply.github.com>
2021-11-29 17:06:58 +0000

    Final visualisations pre-evaluations

--- Matlab/bar_attendance_performing_dates_2019.m
+++ Matlab/bar_attendance_performing_dates_2019.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc
 
-tbl = readtable('heatmap_attendance_performing_dates_2019.csv');
+tbl = readtable('bar_attendance_performing_dates_2019.csv');
 vals = [2019,06,02; 2019,06,16];
 y = [66089 74104; 66386 64689];
 x = datetime(vals,'InputFormat','yyyy,MM,dd');
